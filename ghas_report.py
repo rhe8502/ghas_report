37ec4a10dea2a6c68cb8df2e4d6f74ed8b925e9b
rhe <rhe8502@pm.me>
2023-03-13 03:18:04 +0100

    Add error handling, add projects

--- ghas_report.py
+++ ghas_report.py
@@ -7,10 +7,11 @@ from datetime import datetime
 
 # Load configuration from config.json 
 def load_config():
+    org_names = []
+
     try:
         with open('config.json', 'r') as config_file:
             config = json.load(config_file)
-            
     except FileNotFoundError:
         print('Error: config.json file not found')
         exit(1)
@@ -20,28 +21,27 @@ def load_config():
     
     api_url = config['connection']['gh_api_url']
     api_key = config['connection']['gh_api_key']
-    org_names = config['organizations']['gh_org_names']
+    
+    for project in config.get("project", {}).values():
+        org_names += project.get("organizations", [])
 
     return api_url, api_key, org_names
 
 # Handle API error responses
-def api_error_response(response, org_names):
+def api_error_response(response, org_name,):
     if response.status_code == 401:
         print("Error: Authentication failed. Please check your API key.")
         exit(1)
     elif response.status_code == 403:
-        print(f"Error: Access to organization {org_names} is forbidden. Please check your API key permissions.")
-        return
+        return(f"Error: Access to organization {org_name} is forbidden. Please check your API key permissions.")
     elif response.status_code == 404:
-        print(f"Error: Organization {org_names} not found.")
-        return
+        return(f"Error: Organization {org_name} not found.")
     else:
-        print(f"Error getting alerts for {org_names}: {response.status_code}")
-        return
-
-# Get Code Scanning alerts for an organization
-def get_code_scanning_alerts(api_url, api_key, org_names):
-    url = f"{api_url}/orgs/{org_names}/code-scanning/alerts?state=open"
+        return(f"Error getting alerts for {org_name}: {response.status_code}")  
+    
+# Get Code Scanning alerts and alert count
+def get_code_scanning_alerts(api_url, api_key, org_name):
+    url = f"{api_url}/orgs/{org_name}/code-scanning/alerts?state=open"
     headers = {
         "Authorization" : f"token {api_key}"
     }
@@ -52,14 +52,16 @@ def get_code_scanning_alerts(api_url, api_key, org_names):
         code_scanning_alerts = response.json()
         code_scanning_alert_count = len(code_scanning_alerts)
     else:
-        api_error_response(response, org_names)
-        return
-    
+        error_msg = api_error_response(response, org_name)
+        if error_msg is not None:
+            print(error_msg)
+            return
+                
     return code_scanning_alerts, code_scanning_alert_count
 
-# Get number of Secret Scanning alerts for an organization
-def get_secret_scanning_alerts(api_url, api_key, org_names):
-    url = f"{api_url}/orgs/{org_names}/secret-scanning/alerts?state=open"
+# Get Secret Scanning alerts and alert count
+def get_secret_scanning_alerts(api_url, api_key, org_name):
+    url = f"{api_url}/orgs/{org_name}/secret-scanning/alerts?state=open"
     headers = {
         "Authorization" : f"token {api_key}"
     }
@@ -70,14 +72,16 @@ def get_secret_scanning_alerts(api_url, api_key, org_names):
         secret_scanning_alerts = response.json()
         secret_scanning_alert_count = len(secret_scanning_alerts)
     else:
-        api_error_response(response, org_names)
-        return
+        error_msg = api_error_response(response, org_name)
+        if error_msg is not None:
+            print(error_msg)
+            return
 
-    return secret_scanning_alerts, secret_scanning_alert_count,
+    return secret_scanning_alerts, secret_scanning_alert_count
 
-# Get number of open Dependabot alerts for an organization
-def get_dependabot_alerts(api_url, api_key, org_names):
-    url = f"{api_url}/orgs/{org_names}/dependabot/alerts?state=open"
+# Get Dependabot alerts and alert count
+def get_dependabot_alerts(api_url, api_key, org_name):
+    url = f"{api_url}/orgs/{org_name}/dependabot/alerts?state=open"
     headers = {
         "Authorization" : f"token {api_key}"
     }
@@ -88,8 +92,10 @@ def get_dependabot_alerts(api_url, api_key, org_names):
         dependabot_alerts = response.json()
         dependabot_alert_count = len(dependabot_alerts)  # get the total number of open Dependabot alerts
     else:
-        api_error_response(response, org_names)
-        return
+        error_msg = api_error_response(response, org_name)
+        if error_msg is not None:
+            print(error_msg)
+            return
 
     return dependabot_alerts, dependabot_alert_count
 
@@ -106,7 +112,6 @@ def write_codeql_alerts(codeql_alerts):
     try:
         with open(filename, 'w', newline='') as f:
             writer = csv.writer(f)
-            print("\nWriting CodeQL findings.", end="")
             for codeql_alert in codeql_alerts:
                 writer.writerow(codeql_alert)
             print("\n")
@@ -118,30 +123,33 @@ def get_codql_alerts(api_url, api_key, org_names):
     codeql_alerts = []
 
     for org_name in org_names:
-        alerts = (get_code_scanning_alerts(api_url, api_key, org_name)[0])
-        if len(alerts) > 0:
-            for alert in alerts:
-                codeql_alerts.append([
-                    org_name,
-                    # alert['number'],
-                    datetime.strptime(alert['created_at'], "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d"),
-                    datetime.strptime(alert['updated_at'], "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d"),
-                    # alert['rule']['severity'],
-                    alert['rule']['security_severity_level'],
-                    alert['rule']['id'],
-                    # alert['rule']['description'],
-                    alert['most_recent_instance']['message']['text'],
-                    alert['repository']['name'],
-                    alert['most_recent_instance']['location']['path'],
-                    alert['most_recent_instance']['category'],
-                    alert['html_url']
-                    ])
+        if org_name != "":
+            try:
+                alerts = (get_code_scanning_alerts(api_url, api_key, org_name)[0])
+                if len(alerts) > 0:
+                    for alert in alerts:
+                        codeql_alerts.append([
+                            org_name,
+                            # alert['number'],
+                            datetime.strptime(alert['created_at'], "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d"),
+                            datetime.strptime(alert['updated_at'], "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d"),
+                            # alert['rule']['severity'],
+                            alert['rule']['security_severity_level'],
+                            alert['rule']['id'],
+                            # alert['rule']['description'],
+                            alert['most_recent_instance']['message']['text'],
+                            alert['repository']['name'],
+                            alert['most_recent_instance']['location']['path'],
+                            alert['most_recent_instance']['category'],
+                            alert['html_url']
+                            ])
+            except:
+                pass
 
     return codeql_alerts
 
 # Write open alert count to a CSV file
 def write_alert_count(alert_count):
-
     # Get the current date and time (needed for the filename)
     now = datetime.now()
     filename = now.strftime("open_alert_count_%Y%m%d%H%M%S.csv")
@@ -153,7 +161,6 @@ def write_alert_count(alert_count):
     try:
         with open(filename, 'w', newline='') as f:
             writer = csv.writer(f)
-            print("Writing Vulnerability count.")
             for alerts in alert_count:
                 writer.writerow(alerts)
             print("\n")
@@ -166,15 +173,21 @@ def get_alert_count(api_url, api_key, org_names):
     alert_count = []
 
     for org_name in org_names:
-        alert_count.append([org_name, get_code_scanning_alerts(api_url, api_key, org_name)[1], get_secret_scanning_alerts(api_url, api_key, org_name)[1], get_dependabot_alerts(api_url, api_key, org_name)[1]])
-
-    return alert_count
+        if org_name != "":
+            try:
+                alert_count.append([org_name, get_code_scanning_alerts(api_url, api_key, org_name)[1], get_secret_scanning_alerts(api_url, api_key, org_name)[1], get_dependabot_alerts(api_url, api_key, org_name)[1]])
+            except:
+                pass
+            
+    return alert_count   
 
 # Main function
 def main():
     api_url, api_key, org_names = load_config()
     write_alert_count(get_alert_count(api_url, api_key, org_names))
     write_codeql_alerts(get_codql_alerts(api_url, api_key, org_names))
-    
+
+    # get_alert_count(api_url, api_key, org_names)
+
 if __name__ == '__main__':
     main()
