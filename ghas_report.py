00fc3663f0179eddf3282abc8204faa4e5536553
rhe <rhe8502@pm.me>
2023-03-14 00:28:20 +0100

    Rewrite, reports written with project prefix

--- ghas_report.py
+++ ghas_report.py
@@ -5,35 +5,6 @@ import csv
 import json
 from datetime import datetime
 
-# Load configuration from config.json 
-def load_config():
-    project_names = []
-    org_names = []
-
-    try:
-        with open('config.json', 'r') as config_file:
-            config = json.load(config_file)
-    except FileNotFoundError:
-        print('Error: config.json file not found')
-        exit(1)
-    except json.JSONDecodeError:
-        print('Error: config.json file is not valid JSON')
-        exit(1)
-    
-    api_url = config['connection']['gh_api_url']
-    api_key = config['connection']['gh_api_key']
-
-    for project in config.get("project", {}).keys():
-        project_names.append(project)
-    print(project_names)
-
-    for project in config.get("project", {}).values():
-        org_names += [org for org in project.get("organizations", []) if org != ""]
-    print(org_names)
-
-    return config, api_url, api_key
-    # return api_url, api_key, project_names, org_names
-
 # Handle API error responses
 def api_error_response(response, org_name,):
     if response.status_code == 401:
@@ -48,14 +19,19 @@ def api_error_response(response, org_name,):
         return(f"Error: Organization {org_name} not found.")
     else:
         return(f"Error getting alerts for {org_name}: {response.status_code}")  
-    
+
 # Get Code Scanning alerts and alert count
-def get_code_scanning_alerts(api_url, api_key, org_name):
-    url = f"{api_url}/orgs/{org_name}/code-scanning/alerts?state=open"
+def get_code_scanning_alerts(api_url, api_key, org_name=None, owner=None, repo_name=None):
+    if repo_name:
+        url = f"{api_url}/repos/{owner}/{repo_name}/code-scanning/alerts?state=open"
+    elif org_name:
+        url = f"{api_url}/orgs/{org_name}/code-scanning/alerts?state=open"
+   
     headers = {
         "Authorization" : f"token {api_key}"
     }
-
+    
+    print("URL: ", url)
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
@@ -70,12 +46,17 @@ def get_code_scanning_alerts(api_url, api_key, org_name):
     return code_scanning_alerts, code_scanning_alert_count
 
 # Get Secret Scanning alerts and alert count
-def get_secret_scanning_alerts(api_url, api_key, org_name):
-    url = f"{api_url}/orgs/{org_name}/secret-scanning/alerts?state=open"
+def get_secret_scanning_alerts(api_url, api_key, org_name=None, owner=None, repo_name=None):
+    if repo_name:       
+        url = f"{api_url}/repos/{owner}/{repo_name}/secret-scanning/alerts?state=open"
+    elif org_name:
+        url = f"{api_url}/orgs/{org_name}/secret-scanning/alerts?state=open"
+    
     headers = {
         "Authorization" : f"token {api_key}"
-    }
+    } 
 
+    print("URL: ", url)
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
@@ -90,12 +71,17 @@ def get_secret_scanning_alerts(api_url, api_key, org_name):
     return secret_scanning_alerts, secret_scanning_alert_count
 
 # Get Dependabot alerts and alert count
-def get_dependabot_alerts(api_url, api_key, org_name):
-    url = f"{api_url}/orgs/{org_name}/dependabot/alerts?state=open"
+def get_dependabot_alerts(api_url, api_key, org_name=None, owner=None, repo_name=None):
+    # print("Hello Dependabot Scan", repo_name, " ", owner)
+    if repo_name:
+        url = f"{api_url}/repos/{owner}/{repo_name}/dependabot/alerts?state=open"
+    elif org_name:
+        url = f"{api_url}/orgs/{org_name}/dependabot/alerts?state=open"
     headers = {
         "Authorization" : f"token {api_key}"
     }
 
+    print("URL: ", url)
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
@@ -129,6 +115,50 @@ def write_codeql_alerts(codeql_alerts):
         print("Error: I/O error")
         exit(1)
 
+# Write alert counts to a CSV file
+def write_alert_count_csv(alert_count, project_name):
+    # Get the current date and time (needed for the filename)
+    now = datetime.now()
+    filename = f"{project_name}-open_alerts-{now.strftime('%Y%m%d%H%M%S')}.csv"
+
+    # Write the header row
+    alert_count.insert(0, ['Organization', "Repository", 'Code Scanning Alerts', 'Secret Scanning Alerts', 'Dependabot Alerts'])
+
+    # Write the alert counts to a CSV file
+    try:
+        with open(filename, 'w', newline='') as f:
+            writer = csv.writer(f)
+            for alerts in alert_count:
+                writer.writerow(alerts)
+            print(f"Successfully wrote alert count for project {project_name} to file {filename}")
+    except IOError:
+        print(f"Error writing alert count for project {project_name} to file {filename}")
+
+# Get alert counts for each organization and add them to a list
+def get_alert_count(api_url, api_key, project_data):
+    alert_count = []
+
+    if "organizations" in project_data:
+            for org_name in project_data["organizations"]:
+                if org_name != "":
+                    try:
+                        alert_count.append([org_name, "", get_code_scanning_alerts(api_url, api_key, org_name=org_name)[1], get_secret_scanning_alerts(api_url, api_key, org_name=org_name)[1], get_dependabot_alerts(api_url, api_key, org_name=org_name)[1]])
+                    except Exception as e:
+                        print(f"Error getting alert count for org: {org_name} - {e}")
+                        pass
+   
+    if "repositories" in project_data:
+        for repo_name in project_data["repositories"]:
+            if repo_name != "":
+                try:
+                    owner = project_data.get("owner")  # use .get() to avoid NoneType error
+                    alert_count.append(["", repo_name, get_code_scanning_alerts(api_url, api_key, owner=owner, repo_name=repo_name)[1], get_secret_scanning_alerts(api_url, api_key, owner=owner, repo_name=repo_name)[1], get_dependabot_alerts(api_url, api_key, owner=owner, repo_name=repo_name)[1]])
+                except Exception as e:
+                    print(f"Error getting alert count for repo: {repo_name} - {e}")
+                    pass
+
+    return alert_count
+
 def get_codql_alerts(api_url, api_key, org_names):
     codeql_alerts = []
 
@@ -156,92 +186,30 @@ def get_codql_alerts(api_url, api_key, org_names):
             pass
 
     return codeql_alerts
-'''
-# Write open alert count to a CSV file
-def write_alert_count(alert_count):
-    # Get the current date and time (needed for the filename)
-    now = datetime.now()
-    filename = now.strftime("open_alert_count_%Y%m%d%H%M%S.csv")
-    # print(project_name)
-
-    # Write the header row
-    alert_count.insert(0, ['Organization', 'Code Scanning Alerts', 'Secret Scanning Alerts', 'Dependabot Alerts'])
-
-    # Write the alert counts to a CSV file
-    try:
-        with open(filename, 'w', newline='') as f:
-            writer = csv.writer(f)
-            for alerts in alert_count:
-                writer.writerow(alerts)
-            print("\n")
-    except IOError:
-        print("Error: I/O error")
-        exit(1)
-'''
-
-def write_alert_count(alert_count, project_name):
-    # Get the current date and time (needed for the filename)
-    now = datetime.now()
-    filename = f"{project_name}_open_alert_count_{now.strftime('%Y%m%d%H%M%S')}.csv"
-
-    # Write the header row
-    alert_count.insert(0, ['Organization', 'Code Scanning Alerts', 'Secret Scanning Alerts', 'Dependabot Alerts'])
-
-    # Write the alert counts to a CSV file
-    try:
-        with open(filename, 'w', newline='') as f:
-            writer = csv.writer(f)
-            for alerts in alert_count:
-                writer.writerow(alerts)
-            print(f"Successfully wrote alert count for project {project_name} to file {filename}")
-    except IOError:
-        print(f"Error writing alert count for project {project_name} to file {filename}")
-
-# Get alert counts for a single organization
-def get_alert_count_for_org(api_url, api_key, org_name):
-    try:
-        return [org_name, get_code_scanning_alerts(api_url, api_key, org_name)[1], get_secret_scanning_alerts(api_url, api_key, org_name)[1], get_dependabot_alerts(api_url, api_key, org_name)[1]]
-    except:
-        return [org_name, "this organization does not exist in your GitHub account"]
-
-def get_alert_count_and_write_to_file(api_url, api_key, project_name, project_data):
-    alert_count = []
-    for org_name in project_data["organizations"]:
-        alert_count.append(get_alert_count_for_org(api_url, api_key, org_name))
-    write_alert_count(alert_count, project_name)
-
-'''
-# Get alert counts for each organization and add them to a list
-def get_alert_count(api_url, api_key, org_names):
-    alert_count = []
-
-    for org_name in org_names:
-        try:
-            alert_count.append([org_name, get_code_scanning_alerts(api_url, api_key, org_name)[1], get_secret_scanning_alerts(api_url, api_key, org_name)[1], get_dependabot_alerts(api_url, api_key, org_name)[1]])
-        except:
-            pass
-
-    return alert_count   
-'''
 
 # Main function
 def main():
-    # api_url, api_key, project_names, org_names = load_config()
-    # write_alert_count(get_alert_count(api_url, api_key, org_names))
     # write_codeql_alerts(get_codql_alerts(api_url, api_key, org_names))
-    # get_alert_count(api_url, api_key, org_names)
-
-    # with open("config.json", "r") as input_file:
-    #    data = json.load(input_file)
     
-    config, api_url, api_key = load_config()
+    # Load configuration from config.json file
+    try:
+        with open('config.json', 'r') as config_file:
+            config = json.load(config_file)
+    except FileNotFoundError:
+        print('Error: config.json file not found')
+        exit(1)
+    except json.JSONDecodeError:
+        print('Error: config.json file is not valid JSON')
+        exit(1)
 
-    print(config)
-    exit(0)
+    # Get API URL and API key from config    
+    api_url = config['connection']['gh_api_url']
+    api_key = config['connection']['gh_api_key']
 
+    # Get alert counts for each project and write them to a CSV file
     for project_name, project_data in config["project"].items():
-        print(project_name, " --- ",project_data)
-        # get_alert_count_and_write_to_file(api_url, api_key, project_name, project_data)
+        if project_name != "":
+            write_alert_count_csv(get_alert_count(api_url, api_key, project_data), project_name)
 
 if __name__ == '__main__':
     main()
