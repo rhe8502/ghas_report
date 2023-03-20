90b71bf6693d0d7b85487af69a7a2a905c690aac
rhe <rhe8502@pm.me>
2023-03-20 01:11:50 +0100

    Add license header

--- ghas_report.py
+++ ghas_report.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
+#
+# Copyright (c) 2023 Rupert Herbst <rhe8502(at)pm.me>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Rupert Herbst <rhe8502(at)pm.me>
+# Package: ghas_report.py
+# Version: x.x.x
+# Project URL: https://github.com/rhe8502/ghas_report/
+
+"""
+GitHub Advanced Security (GHAS) Vulnerability Report
+"""
 
 import requests
 import csv
