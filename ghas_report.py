0ff12c47e52a8ddfcfefd3f831dfc89e70934708
rhe <rhe8502@pm.me>
2023-03-24 04:47:50 +0100

    Specify either json, or CSV file output, reduce to one write function

--- ghas_report.py
+++ ghas_report.py
@@ -95,73 +95,30 @@ def get_dependabot_alerts(api_url, org_name=None, owner=None, repo_name=None):
     else:
         print(api_error_response(response))
 
-# Write alert count to a CSV file
-def write_alert_count_csv(alert_count, project_name):
+# Write alerts to file
+def write_alerts(alert_data, project_name, output_type=None, calling_function=None):    
     now = datetime.now()
-    filename = f"{project_name}-alert_count-{now.strftime('%Y%m%d%H%M%S')}.csv"
+    output_type = output_type if output_type is not None else 'csv'
+    filename = f"{project_name}-{calling_function}-{now.strftime('%Y%m%d%H%M%S')}.{output_type}"
 
-    # Write the header row
-    alert_count.insert(0, ['Organization', "Repository", 'Code Scanning Alerts', 'Secret Scanning Alerts', 'Dependabot Alerts'])
+    scan_options = {
+        'alert_count': ['Organization', 'Repository', 'Code Scanning Alerts', 'Secret Scanning Alerts', 'Dependabot Alerts'],
+        'code_scan': ['Organization', 'Repository', 'Date Created', 'Date Updated', 'Severity', 'Rule ID', 'Description', 'File', 'Category', 'GitHub URL'],
+        'secret_scan': ['Organization', 'Repository', 'Date Created', 'Date Updated', 'Secret Type Name', 'Secret Type', 'GitHub URL'],
+        'dependabot_scan': ['Organization', 'Repository', 'Date Created', 'Date Updated', 'Severity', 'Package Name', 'CVE ID', 'Summary', 'Scope', 'Manifest ID', 'GitHub URL']
+    }
 
     try:
-        with open(filename, 'w', newline='') as f:
-            writer = csv.writer(f)
-            for alerts in alert_count:
-                writer.writerow(alerts)
-            print(f"Successfully wrote alert count for project \"{project_name}\" to file {filename}")
-    except IOError:
-        print(f"Error writing alert count for project {project_name} to file {filename}")
-
-# Write code scan findings to a CSV file
-def write_codescan_alerts_csv(codescan_alerts, project_name):    
-    now = datetime.now()
-    filename = f"{project_name}-codescan_alerts-{now.strftime('%Y%m%d%H%M%S')}.csv"
-
-    # Write the header row
-    codescan_alerts.insert(0, ['Organization', 'Repository', 'Date Created', 'Date Updated', 'Severity', 'Rule ID', 'Description', 'File', 'Category', 'GitHub URL'])
-
-    try:
-        with open(filename, 'w', newline='') as f:
-            writer = csv.writer(f)
-            for codeql_alert in codescan_alerts:
-                writer.writerow(codeql_alert)
-            print(f"Successfully wrote code scan findings for project \"{project_name}\" to file {filename}")
-    except IOError:
-        print("Error: I/O error")
-        exit(1)
-
-# Write secret scan findings to a CSV file
-def write_secretscan_alerts_csv(secretscan_alerts, project_name):    
-    now = datetime.now()
-    filename = f"{project_name}-secretscan_alerts-{now.strftime('%Y%m%d%H%M%S')}.csv"
-
-    # Write the header row
-    secretscan_alerts.insert(0, ['Organization', 'Repository', 'Date Created', 'Date Updated', 'Secret Type Name', 'Secret Type', 'GitHub URL'])
-
-    try:
-        with open(filename, 'w', newline='') as f:
-            writer = csv.writer(f)
-            for secretscan_alert in secretscan_alerts:
-                writer.writerow(secretscan_alert)
-            print(f"Successfully wrote secret scan findings for project \"{project_name}\" to file {filename}")
-    except IOError:
-        print("Error: I/O error")
-        exit(1)
-
-# Write DependaBot scan findings to a CSV file
-def write_dependabot_alerts_csv(dependabot_alerts, project_name):    
-    now = datetime.now()
-    filename = f"{project_name}-dependabot_alerts-{now.strftime('%Y%m%d%H%M%S')}.csv"
-
-    # Write the header row
-    dependabot_alerts.insert(0, ['Organization', 'Repository', 'Date Created', 'Date Updated', 'Severity', 'Package Name', 'CVE ID', 'Summary', 'Scope', 'Manifest ID', 'GitHub URL'])
-
-    try:
-        with open(filename, 'w', newline='') as f:
-            writer = csv.writer(f)
-            for dependabot_alert in dependabot_alerts:
-                writer.writerow(dependabot_alert)
-            print(f"Successfully wrote Dependabot scan findings for project \"{project_name}\" to file {filename}")
+        with open(filename, 'w', encoding='utf-8', newline='') as f:
+            if output_type == 'json':
+                json.dump(alert_data["raw_alerts"], f, indent=4)
+            elif output_type == 'csv':
+                writer = csv.writer(f)
+                header_row = scan_options.get(calling_function, scan_options['code_scan'])
+                writer.writerow(header_row)
+                for scan_alert in alert_data["scan_alerts"]:
+                    writer.writerow(scan_alert)  
+            print(f"Successfully wrote {calling_function} findings for project \"{project_name}\" to file {filename}")
     except IOError:
         print("Error: I/O error")
         exit(1)
@@ -181,12 +138,12 @@ def alert_count(api_url, project_data):
                         alert_count.append(["N/A", gh_name, get_code_scanning_alerts(api_url, owner=owner, repo_name=gh_name)[1], get_secret_scanning_alerts(api_url, owner=owner, repo_name=gh_name)[1], get_dependabot_alerts(api_url, owner=owner, repo_name=gh_name)[1]])
                 except Exception as e:
                     print(f"Error getting alert count for {'repository' if gh_entity == 'repositories' else 'organization'}: {gh_name} - {e}")
-    return alert_count
+    return {"raw_alerts": alert_count, "scan_alerts": alert_count}
 
-
-# Process code scan alerts for each organization and repository and add them to a list
+# Process code scanning alerts for each organization and repository and add them to a list
 def code_scanning_alerts(api_url, project_data):
-    codescan_alerts = []
+    raw_alerts = []
+    scan_alerts = []
 
     for gh_entity in ['organizations', 'repositories']:
         for gh_name in project_data.get(gh_entity, []):
@@ -195,7 +152,8 @@ def code_scanning_alerts(api_url, project_data):
                     owner = project_data.get('owner') if gh_entity == 'repositories' else None
                     alerts = get_code_scanning_alerts(api_url, owner=owner, org_name=gh_name if gh_entity == 'organizations' else None, repo_name=gh_name if gh_entity == 'repositories' else None)[0]
                     for alert in alerts:
-                        codescan_alerts.append([
+                        raw_alerts.append(alert)
+                        scan_alerts.append([
                             gh_name if gh_entity == 'organizations' else alert.get('organization', {}).get('name', "N/A"),
                             gh_name if gh_entity == 'repositories' else alert.get('repository', {}).get('name', "N/A"),
                             datetime.strptime(alert.get('created_at', "N/A"), "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d"),
@@ -209,11 +167,12 @@ def code_scanning_alerts(api_url, project_data):
                         ])
                 except Exception as e:
                     print(f"Error getting CodeQL alerts for {'repository' if gh_entity == 'repositories' else 'organization'}: {gh_name} - {e}")
-    return codescan_alerts
+    return {"raw_alerts": raw_alerts, "scan_alerts": scan_alerts}
 
 # Process Secret Scanning alerts for each organization and repository and add them to a list
 def secret_scanning_alerts(api_url, project_data):
-    secretscan_alerts = []
+    raw_alerts = []
+    scan_alerts = []
 
     for gh_entity in ['organizations', 'repositories']:
         for gh_name in project_data.get(gh_entity, []):
@@ -222,7 +181,8 @@ def secret_scanning_alerts(api_url, project_data):
                     owner = project_data.get('owner') if gh_entity == 'repositories' else None
                     alerts = get_secret_scanning_alerts(api_url, owner=owner, org_name=gh_name if gh_entity == 'organizations' else None, repo_name=gh_name if gh_entity == 'repositories' else None)[0]
                     for alert in alerts:
-                        secretscan_alerts.append([
+                        raw_alerts.append(alert)
+                        scan_alerts.append([
                             gh_name if gh_entity == 'organizations' else alert.get('organization', {}).get('name', "N/A"),
                             gh_name if gh_entity == 'repositories' else alert.get('repository', {}).get('name', "N/A"),
                             alert.get('repository', {}).get('name', "N/A"),
@@ -234,11 +194,12 @@ def secret_scanning_alerts(api_url, project_data):
                         ])
                 except Exception as e:
                     print(f"Error getting secret scanning alerts for {'repository' if gh_entity == 'repositories' else 'organization'}: {gh_name} - {e}")
-    return secretscan_alerts
+    return {"raw_alerts": raw_alerts, "scan_alerts": scan_alerts}
 
 # Process Dependabot alerts for each organization and repository and add them to a list
 def dependabot_scanning_alerts(api_url, project_data):
-    dependabot_alerts = []
+    raw_alerts = []
+    scan_alerts = []
 
     for gh_entity in ['organizations', 'repositories']:
         for gh_name in project_data.get(gh_entity, []):
@@ -247,7 +208,8 @@ def dependabot_scanning_alerts(api_url, project_data):
                     owner = project_data.get('owner') if gh_entity == 'repositories' else None
                     alerts = get_dependabot_alerts(api_url, owner=owner, org_name=gh_name if gh_entity == 'organizations' else None, repo_name=gh_name if gh_entity == 'repositories' else None)[0]
                     for alert in alerts:
-                        dependabot_alerts.append([
+                        raw_alerts.append(alert)
+                        scan_alerts.append([
                             gh_name if gh_entity == 'organizations' else alert.get('organization', {}).get('name', "N/A"),
                             gh_name if gh_entity == 'repositories' else alert.get('repository', {}).get('name', "N/A"),
                             datetime.strptime(alert.get('created_at', "N/A"), "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d"),
@@ -262,7 +224,7 @@ def dependabot_scanning_alerts(api_url, project_data):
                         ])
                 except Exception as e:
                     print(f"Error getting dependabot alerts for {'repository' if gh_entity == 'repositories' else 'organization'}: {gh_name} - {e}")
-    return dependabot_alerts
+    return {"raw_alerts": raw_alerts, "scan_alerts": scan_alerts}
 
 def main():
     # Load configuration from ghas_config.json file
@@ -295,7 +257,7 @@ It will generate a report based on the specified options and write the results t
     ''', formatter_class=argparse.RawTextHelpFormatter)
 
     #Options group
-    parser.add_argument('-V', '--version', action='version', version='%(prog)s 0.1', help='show program\'s version number and exit')
+    parser.add_argument('-V', '--version', action='version', version='%(prog)s v0.1', help='show program\'s version number and exit')
 
     # Alert reports
     alert_group = parser.add_argument_group('Generate alert reports')
@@ -308,68 +270,57 @@ It will generate a report based on the specified options and write the results t
     # Optional alert reports arguments
     alert_options_group = parser.add_argument_group('Optional alert report arguments')
     alert_options_group.add_argument('-v', '--verbose', action='store_true', help='write all information to the output file (only applicable for CSV files)')
-    alert_options_group.add_argument('-o', '--open', action='store_true', help='only generate reports for open alerts (Alert Count only reports open alerts by default)')
-    alert_options_group.add_argument('-g', '--org', metavar='ORG', help='specify the organization to generate a report for')
-    alert_options_group.add_argument('-r', '--repo', metavar='REPO', help='specify the repository to generate a report for')
+    # alert_options_group.add_argument('-o', '--open', action='store_true', help='only generate reports for open alerts (Alert Count only reports open alerts by default)')
+    # alert_options_group.add_argument('-g', '--org', metavar='ORG', help='specify the organization to generate a report for')
+    # alert_options_group.add_argument('-r', '--repo', metavar='REPO', help='specify the repository to generate a report for')
 
     # Optional arguments
     optional_group = parser.add_argument_group('Optional arguments')
    
     # Output file format arguments
     output_group = parser.add_argument_group('Output file format arguments')
-    output_group.add_argument('-wC', '--output-csv', action='store_true', help='write output to a CSV file (default format)')
-    output_group.add_argument('-wP', '--output-pdf', action='store_true', help='write output to a PDF file')
-    output_group.add_argument('-wH', '--output-html', action='store_true', help='write output to a HTML file')
-    output_group.add_argument('-wJ', '--output-json', action='store_true', help='write output to a JSON file')
     output_group.add_argument('-wA', '--output-all', action='store_true', help='write output to all formats at once')
+    output_group.add_argument('-wC', '--output-csv', action='store_true', help='write output to a CSV file (default format)')
+    output_group.add_argument('-wJ', '--output-json', action='store_true', help='write output to a JSON file')
+    # output_group.add_argument('-wP', '--output-pdf', action='store_true', help='write output to a PDF file')
+    # output_group.add_argument('-wH', '--output-html', action='store_true', help='write output to a HTML file')
 
     # Report type
-    report_group = parser.add_argument_group('Report type (only applicable for PDF and HTML output formats)')
-    report_group.add_argument('-Ro', '--report-owasp', action='store_true', help=' OWASP Top 10 - 2021')
+    # report_group = parser.add_argument_group('Report type (only applicable for PDF and HTML output formats)')
+    # report_group.add_argument('-Ro', '--report-owasp', action='store_true', help=' OWASP Top 10 - 2021')
 
     # Optional file arguments
-    optional_file_group = parser.add_argument_group('Optional file arguments')
-    optional_file_group.add_argument('-D', '--dir', metavar='DIR', help='specify the directory to write the output to. If none specified, the current directory is used.')
-    optional_file_group.add_argument('-C', '--config', metavar='CON', default='ghas_config.json', help='specify a config file to use. If none specified "ghas_config.json" is used. If ghas_config.json is not found in the current directory, a new config file will be created.\n\n')
+    # optional_file_group = parser.add_argument_group('Optional file arguments')
+    # optional_file_group.add_argument('-D', '--dir', metavar='DIR', help='specify the directory to write the output to. If none specified, the current directory is used.')
+    # optional_file_group.add_argument('-C', '--config', metavar='CON', default='ghas_config.json', help='specify a config file to use. If none specified "ghas_config.json" is used. If ghas_config.json is not found in the current directory, a new config file will be created.\n\n')
 
     # Parse the arguments
     args = parser.parse_args()
 
     # Define the list of alert types to process. If the -A flag is present, include all alert types. Otherwise, include only the alert types that were passed as arguments
     alert_types = ['alerts', 'codescan', 'secretscan', 'dependabot'] if args.all else [t for t in ['alerts', 'codescan', 'secretscan', 'dependabot'] if getattr(args, t)]
+    
+    # Define the list of output types to process. If the -wA flag is present, include all output types. Otherwise, include only the output types that were passed as arguments
+    output_types = ['csv', 'json'] if args.output_all else [t for t in ['csv', 'json'] if getattr(args, f'output_{t}')]
+
+    # Set CSV as the default output type if no output type is specified
+    if not output_types:
+        output_types = ['csv']
 
     if not alert_types:
         print('\nError: No alert type specified.\n')
         parser.print_help()
-
     else:
         # Process each project for the selected alert types
         for project_name, project_data in config.get('projects', {}).items():
             for alert_type in alert_types:
-                {
-                    'alerts': lambda: write_alert_count_csv(alert_count(api_url, project_data), project_name),
-                    'codescan': lambda: write_codescan_alerts_csv(code_scanning_alerts(api_url, project_data), project_name),
-                    'secretscan': lambda: write_secretscan_alerts_csv(secret_scanning_alerts(api_url, project_data), project_name),
-                    'dependabot': lambda: write_dependabot_alerts_csv(dependabot_scanning_alerts(api_url, project_data), project_name),
-                }[alert_type]()
-
-    '''
-    # Get Alert count for each project and write them to a CSV file
-    for project_name, project_data in config.get('projects', {}).items():
-        write_alert_count_csv(alert_count(api_url, project_data), project_name)
-    
-    # Get Code scan findings for each organization and write them to a CSV file
-    for project_name, project_data in config.get('projects', {}).items():
-        write_codescan_alerts_csv(code_scanning_alerts(api_url, project_data), project_name)
-    
-    # Get Secret scan findings for each organization and write them to a CSV file
-    for project_name, project_data in config.get('projects', {}).items():
-        write_secretscan_alerts_csv(secret_scanning_alerts(api_url, project_data), project_name)
-   
-    # Get Dependabot scan findings for each organization and write them to a CSV file
-    for project_name, project_data in config.get('projects', {}).items():
-        write_dependabot_alerts_csv(dependabot_scanning_alerts(api_url, project_data), project_name)
-    '''
+                for output_type in output_types:
+                    {
+                        'alerts': lambda: write_alerts(alert_count(api_url, project_data), project_name, output_type, calling_function='alert_count'),
+                        'codescan': lambda: write_alerts(code_scanning_alerts(api_url, project_data), project_name, output_type, calling_function='code_scan'),
+                        'secretscan': lambda: write_alerts(secret_scanning_alerts(api_url, project_data), project_name, output_type, calling_function='secret_scan'),
+                        'dependabot': lambda: write_alerts(dependabot_scanning_alerts(api_url, project_data), project_name, output_type, calling_function='dependabot_scan'),
+                    }[alert_type]()
             
 if __name__ == '__main__':
     main()
